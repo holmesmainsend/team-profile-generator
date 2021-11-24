7f8b75669b661c5f1eff63be305f15e54214ce0d
Mona Lisa <holmesmainsend@gmail.com>
2021-11-24 10:23:36 -0500

    passed all manager tests and began engineer tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -4,10 +4,11 @@ const Employee = require('../lib/Employee');
 test('creates an employee object', () => {
     const employee = new Employee('Harold');
 
+    expect(employee.role).toBe('Employee');
     expect(employee.name).toBe('Harold');
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
-    expect(employee.role).toEqual(expect.any(String));
+    console.log(employee);
 })
 
 
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -3,18 +3,20 @@ const Manager = require('../lib/Manager');
 test('creates a manager object', () => {
     const manager = new Manager('Harold');
 
+    expect(manager.role).toBe('Manager');
     expect(manager.name).toBe('Harold');
     expect(manager.id).toEqual(expect.any(Number));
     expect(manager.email).toEqual(expect.any(String));
-    expect(manager.role).toEqual(expect.any(String));
+    expect(manager.officeNumber).toEqual(expect.any(Number));
+    console.log(manager);
 })
 
 
-// test('gets manager details', () => {
-//     const manager = new Manager('Harold');
+test('gets manager details', () => {
+    const manager = new Manager('Harold');
 
-//     expect(manager.getName()).toHaveProperty('name');
-//     expect(manager.getId()).toHaveProperty('id');
-//     expect(manager.getEmail()).toHaveProperty('email');
-//     expect(manager.getRole()).toHaveProperty('role');
-// })
+    expect(manager.getName()).toHaveProperty('name');
+    expect(manager.getId()).toHaveProperty('id');
+    expect(manager.getEmail()).toHaveProperty('email');
+    expect(manager.getRole()).toHaveProperty('role');
+})
