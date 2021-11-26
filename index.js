2bc4247b84d1b06fedc999b7cbdcd04d08ca3905
Mona Lisa <holmesmainsend@gmail.com>
2021-11-26 11:33:08 -0500

    updated TODOs and tested array setups

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
+// TODO: create mocks to re-pass all tests
+// TODO: create for loop (or forEach) based on length of arrays to pass in values to fs; remove hardcoded Jeffs, Bronsons, and Eugenes
+// TODO: account for empty intern and/or engineer array
+// TODO: add number/string validation for inquirer
+// TODO: account for spaces in inquirer answers(?)
+// TODO: create HTML file using fs
+// TODO: add stylesheet + styling (bootstrap?)
+// TODO: change console log message upon finalization
+// TODO: create video and create sample HTML page in process
+// TODO: add README using README generator
+
 const fs = require("fs");
 const inquirer = require("inquirer");
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
-// const Bronson = new Engineer ("Bronson", 33, "engine@engine.net", "gitter@github.com");
-// const Eugene = new Intern ("Eugene", 10, "eugy@hotmail.com", "URI");
-// console.log(Bronson, Eugene);
-
 // Function to initialize app
 function initManager() {
-  teamArray = [];
+  managerArray = [];
+  engineerArray = [];
+  internArray = [];
+
   return inquirer
   .prompt([
     {
       name: "name",
-      message: "Manager Name: ",
+      message: "Manager First Name: ",
       validate: (managerName) => {
         if (managerName) {
           return true;
         } else {
-          console.log("Please enter a manager name");
+          console.log("Please enter a manager first name");
           return false;
         }
       },
@@ -70,17 +80,14 @@ function initManager() {
   ])
   .then((data) => {
       if (data.continuation === "Engineer") {
-        teamArray.push(data)
-        console.log(teamArray)
+        managerArray.push(data)
         initEngineer();
       } else if (data.continuation === "Intern") {
-        teamArray.push(data)
-        console.log(teamArray)
+        managerArray.push(data)
         initIntern();
       } else {
-        teamArray.push(data)
-        let Jeff = new Manager(teamArray[0].name, teamArray[0].id, teamArray[0].email, teamArray[0].officeNumber);
-        console.log(Jeff);
+        managerArray.push(data)
+        let Jeff = new Manager(managerArray[0].name, managerArray[0].id, managerArray[0].email, managerArray[0].officeNumber);
         console.log("End of team");
       }
   });
@@ -90,19 +97,19 @@ function initEngineer() {
   return inquirer
   .prompt([
     {
-      name: "engineerName",
-      message: "Engineer Name: ",
+      name: "name",
+      message: "Engineer First Name: ",
       validate: (engineerName) => {
         if (engineerName) {
           return true;
         } else {
-          console.log("Please enter an engineer name");
+          console.log("Please enter an engineer first name");
           return false;
         }
       },
     },
     {
-      name: "engineerId",
+      name: "id",
       message: "Employee ID: ",
       validate: (engineerId) => {
         if (engineerId) {
@@ -114,7 +121,7 @@ function initEngineer() {
       },
     },
     {
-      name: "engineerEmail",
+      name: "email",
       message: "Engineer Email: ",
       validate: (engineerEmail) => {
         if (engineerEmail) {
@@ -126,7 +133,7 @@ function initEngineer() {
       },
     },
     {
-      name: "githubUser",
+      name: "github",
       message: "GitHub Username: ",
       validate: (githubUser) => {
         if (githubUser) {
@@ -146,16 +153,16 @@ function initEngineer() {
   ])
   .then((data) => {
     if (data.continuation === "Engineer") {
-      teamArray.push(data)
-      console.log(teamArray)
+      engineerArray.push(data)
       initEngineer();
     } else if (data.continuation === "Intern") {
-      teamArray.push(data)
-      console.log(teamArray)
+      engineerArray.push(data)
       initIntern();
     } else {
-      teamArray.push(data)
-      console.log(teamArray)
+      engineerArray.push(data)
+      let Jeff = new Manager(managerArray[0].name, managerArray[0].id, managerArray[0].email, managerArray[0].officeNumber);
+      let Bronson = new Engineer(engineerArray[0].name, engineerArray[0].id, engineerArray[0].email, engineerArray[0].github);
+      let Eugene = new Intern(internArray[0].name, internArray[0].id, internArray[0].email, internArray[0].school);
       console.log("End of team");
     }
 });
@@ -165,19 +172,19 @@ function initIntern() {
   return inquirer
   .prompt([
     {
-      name: "internName",
-      message: "Intern Name: ",
+      name: "name",
+      message: "Intern First Name: ",
       validate: (internName) => {
         if (internName) {
           return true;
         } else {
-          console.log("Please enter an intern name");
+          console.log("Please enter an intern first name");
           return false;
         }
       },
     },
     {
-      name: "internId",
+      name: "id",
       message: "Employee ID: ",
       validate: (internId) => {
         if (internId) {
@@ -189,7 +196,7 @@ function initIntern() {
       },
     },
     {
-      name: "internEmail",
+      name: "email",
       message: "Intern Email: ",
       validate: (internEmail) => {
         if (internEmail) {
@@ -202,12 +209,12 @@ function initIntern() {
     },
     {
       name: "school",
-      message: "School Name: ",
+      message: "School Name (no spaces): ",
       validate: (school) => {
         if (school) {
           return true;
         } else {
-          console.log("Please enter a school name");
+          console.log("Please enter a school name (no spaces");
           return false;
         }
       },
@@ -221,20 +228,19 @@ function initIntern() {
   ])
   .then((data) => {
     if (data.continuation === "Engineer") {
-      teamArray.push(data)
-      console.log(teamArray)
+      internArray.push(data)
       initEngineer();
     } else if (data.continuation === "Intern") {
-      teamArray.push(data)
-      console.log(teamArray)
+      internArray.push(data)
       initIntern();
     } else {
-      teamArray.push(data)
-      console.log(teamArray)
+      internArray.push(data)
+      let Jeff = new Manager(managerArray[0].name, managerArray[0].id, managerArray[0].email, managerArray[0].officeNumber);
+      let Bronson = new Engineer(engineerArray[0].name, engineerArray[0].id, engineerArray[0].email, engineerArray[0].github);
+      let Eugene = new Intern(internArray[0].name, internArray[0].id, internArray[0].email, internArray[0].school);
       console.log("End of team");
     }
 });
 }
-
 
 initManager();
